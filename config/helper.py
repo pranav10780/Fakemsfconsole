69e7193ca7ddb575d21c03d052a9190e8460fa96
pranav10780 <pranavsuresh107@gmail.com>
2024-04-17 22:30:47 +0530

    Added more help text

--- config/helper.py
+++ config/helper.py
@@ -1,6 +1,12 @@
 def helper(data) -> "":
   match data:
-    case "help":
+    case "help" or "?":
       return "Help is a command which gives a list of commands that can be run."
+    case "exit":
+      return "Exits from the application"
+    case "banner":
+      return "Prints the metaspliot banner"
+    case "connectivity":
+      return "Checks if internet access is available"
     case _:
         return "Sorry unknown command, it can't be helped"
